7eeccdd755a4fdfb3513634c05a9b017d4788c64
davislewis <dl672123@ohio.edu>
2025-10-13 20:22:34 -0400

    should work now on windows and mac

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,14 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    //engine.load(QUrl::fromLocalFile("../src/gui/main.qml"));
-    engine.load(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/../Resources/main.qml"));
 
-
+    #ifdef _WIN32
+        engine.load(QUrl::fromLocalFile("../src/gui/main.qml"));
+    #elif __APPLE__
+        engine.load(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/../Resources/main.qml"));
+    #else
+        std::cout << "Unkown operating system" << std::endl;
+    #endif
 
     if (engine.rootObjects().isEmpty()) {
         return -1;
