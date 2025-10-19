8f4ad8bfedbb23a09a706c8908d3a67cb08101f6
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2025-10-19 12:08:22 -0400

    Tag C++ model

--- src/core/models/tag.h
+++ src/core/models/tag.h
@@ -10,84 +10,15 @@
 
 #include <string>
 
-class Tag {
- public:
-    /**
-     * @brief  initializes id to -1, name to "", and dateCreated to ""
-     *
-     */
-    Tag() : id(-1), name(""), dateCreated("") {}
-    
-    /**
-     * @brief  initializes id to -1, name to newName, dateCreated to ""
-     *
-     * @param newName  name of the tag
-     */
-    Tag(const std::string& newName) : id(-1), name(newName), dateCreated("") {}
+struct Tag {
+    int id = -1;
+    std::string name = "";
+    std::string dateCreated = "";
+    std::string dateUpdated = "";
+    bool showDate = true;
 
-    /**
-     * @brief  initializes id to newId, name to newName, dateCreated to ""
-     *
-     * @param newId  id of the tag
-     * @param newName  name of the tag
-     */
-    Tag(const int& newId, const std::string& newName) : id(newId), name(newName), dateCreated("") {}
-    
-    /**
-     * @brief  initializes id to newId, name to newName, dateCreated to newDate
-     *
-     * @param newId  id of tag
-     * @param newName  name of tag
-     * @param newDate  dateCreated of the tag
-     */
-    Tag(const int& newId, const std::string& newName, const std::string& newDate) : id(newId), name(newName), dateCreated(newDate) {}
-
-    /**
-     * @brief  does not throw an exception. will return -1 if no id is assigned
-     *
-     * @return  id of the tag
-     */
-    int getId() const noexcept { return id; }
-
-    /**
-     * @brief  does not throw an exception. name will be empty string if it does not have one
-     *
-     * @return  const reference to the name
-     */
-    const std::string& getName() const noexcept { return name; }
-
-    /**
-     * @brief  does not throw an exception. date will be an empty string if there is no creation date
-     *
-     * @return  const reference to the dateCreated
-     */
-    const std::string& getDateCreated() const noexcept { return dateCreated; }
-
-    /**
-     * @brief  does not throw an exception. sets the tag id to specified id
-     *
-     * @param newId  id to set
-     */
-    void setId(const int& newId) noexcept { id = newId; }
-
-    /**
-     * @brief  does not throw an exception. sets the name to specified name
-     *
-     * @param newname  name to set
-     */
-    void setName(const std::string& newName) noexcept { name = newName; }
-
-    /**
-     * @brief  does not throw an exception. sets the dateCreated to specified date (in string form)
-     *
-     * @param newDate  dateCreated to set
-     */
-    void setDateCreated(const std::string& newDate) noexcept { dateCreated = newDate; }
-    
- private:
-    int id;
-    std::string name;
-    std::string dateCreated;
+    Tag(int id, std::string name, std::string dateCreated, std::string dateUpdated)
+        : id(id), name(name), dateCreated(dateCreated), dateUpdated(dateUpdated) {}
 };
 
 #endif
