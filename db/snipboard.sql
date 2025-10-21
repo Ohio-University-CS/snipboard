95dc34248f013190c2d74a3e6fa3e966343fbe1b
davislewis <dl672123@ohio.edu>
2025-10-21 17:03:38 -0400

    updated schema and added more default tags

--- db/snipboard.sql
+++ db/snipboard.sql
@@ -1,14 +1,17 @@
+--schema
 create table Tag(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     dateCreated DATE DEFAULT CURRENT_TIMESTAMP,
-    showDate BOOLEAN DEAFULT TRUE --allow the ability to not show the date created on default/non user created tags
+    dateModified DATE DEFAULT 0,
+    userCreated BOOLEAN DEAFULT TRUE 
 );
 
 create table Folder(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     dateCreated DATE DEFAULT CURRENT_TIMESTAMP,
+    dateModified DATE DEFAULT 0,
     parentFolder INTEGER, --only parent is needed because children can be found by searching folders where the parentFolderID is equal to the parent folder
     FOREIGN KEY (parentFolder) REFERENCES Folder(ID)
 );
@@ -17,8 +20,10 @@ create table Snippet(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     dateCreated DATE DEFAULT CURRENT_TIMESTAMP,
+    dateModified DATE DEFAULT 0,
     description TEXT,
     language TEXT,
+    data TEXT,
     tags INTEGER,
     folder INTEGER,
     favorite BOOLEAN NOT NULL DEFAULT FALSE,
@@ -32,8 +37,36 @@ WITH data (name) AS (
     VALUES
         ("Recursive"),
         ("Iterative"),
-        ("Search"),
-        ("Sort")
+        ("Search Algorithm"),
+        ("Sorting Algorithm"),
+        ("Data Structure"),
+        ("String Manipulation"),
+        ("File Handling"),
+        ("Database Query"),
+        ("API Request"),
+        ("Authentication"),
+        ("Error Handling"),
+        ("Test Cases"),
+        ("Logging"),
+        ("Parsing"),
+        ("Serialization"),
+        ("Encryption"),
+        ("Input Validation"),
+        ("Web Development"),
+        ("Front End"),
+        ("Back End"),
+        ("Full Stack"),
+        ("Web Developemnt"),
+        ("Mobile Development"),
+        ("Desktop"),
+        ("Dev Ops"),
+        ("Cloud"),
+        ("Command"),
+        ("Script"),
+        ("Class"),
+        ("Class Definition")
 )   
-INSERT INTO Tag (name, showDate)
+INSERT INTO Tag (name, userCreated)
 SELECT name, FALSE FROM data;
+
+--test data
