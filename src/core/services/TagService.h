bf417036b55aa358ef312c98b6cb342b1ba71bea
davislewis <dl672123@ohio.edu>
2025-11-03 19:57:00 -0500

    implemented tag service

--- src/core/services/TagService.h
+++ src/core/services/TagService.h
@@ -14,3 +14,24 @@
 #include "../objects/TagListModel.h"
 #include "../objects/TagObject.h"
 #include "../repositories/SnippetRepository.h"
+
+class TagService : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(TagListModel* tags READ tags CONSTANT)
+
+    public:
+        explict TagService(QObject* parent = nullptr);
+
+        //QML methods
+        SnippetListModel* tag() { return &m_tagModel; }
+        Q_INVOKABLE void createTag(const QString& name);
+        Q_INVOKABLE void deleteTag(int id);
+        Q_INVOKABLE void updateTag(int id, const QString& name);
+
+    private:
+        void loadTagsFromDb();
+
+        QSqlDatabase m_db;
+        TagRepository* m_repo;
+        TagListModel m_tagModel; // the list mnodel its resposible for
+}
