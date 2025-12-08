c2f9e3e2ff2bb8287f63c7726acea711024789b1
davislewis <dl672123@ohio.edu>
2025-12-07 21:59:41 -0500

    Revert "added getter functions"
    This reverts commit 31248460a3bc0344907f767cbd5473b36e183cf8.

--- src/core/services/SettingsService.h
+++ src/core/services/SettingsService.h
@@ -17,7 +17,6 @@ public:
 
     const Settings& settings() const { return m_settings; }
 
-    //setters
     Q_INVOKABLE void setTheme(int value);
     Q_INVOKABLE void setEditorFontSize(int value);
     Q_INVOKABLE void setEditorFontFamily(int value);
@@ -33,23 +32,6 @@ public:
     Q_INVOKABLE void setExportFormat(QString value);
     Q_INVOKABLE void setImportLocation(QString value);
     Q_INVOKABLE void setConflictHandeling(QString value);
-
-    //getters
-    Q_INVOKABLE int getTheme(){return m_settings.theme;}
-    Q_INVOKABLE int getEditorFontSize(){return m_settings.editorFontSize;}
-    Q_INVOKABLE int getEditorFontFamily(){return m_settings.editorFontFamily;}
-    Q_INVOKABLE bool getLineNumbers(){return m_settings.lineNumbers;}
-    Q_INVOKABLE bool getSyntaxHighlighting(){return m_settings.syntaxHighlighting;}
-    Q_INVOKABLE bool getWrapLines(){return m_settings.wrapLines;}
-    Q_INVOKABLE bool getConfirmBeforeDelete(){return m_settings.confrimBeforeDelete;}
-    Q_INVOKABLE int getTabSize(){return m_settings.tabSize;}
-    Q_INVOKABLE QString getDefaultLanguage(){return m_settings.defaultLanguage;}
-    Q_INVOKABLE QString getDefaultSortMethod(){return m_settings.defaultSortMethod;}
-    Q_INVOKABLE int getDefaultSnippetFolder(){return m_settings.defaultSnippetFolder;}
-    Q_INVOKABLE QString getExportLocation(){return m_settings.exportLocation;}
-    Q_INVOKABLE QString getExportFormat(){return m_settings.exportFormat;}
-    Q_INVOKABLE QString getImportLocation(){return m_settings.importLocation;}
-    Q_INVOKABLE QString getConflictHandeling(){return m_settings.conflictHandeling;}
 
 signals:
     void settingsChanged();
